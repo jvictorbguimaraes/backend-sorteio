b9030012d6943b1a79f2ff62e2173bdcd0b9b72a
jvictorbguimaraes <jvictorbguimaraes@gmail.com>
2018-12-07 21:40:31 -0200

    commit

---
+++ ServerApi/Controllers/SorteioController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ServerApi.Model;
+
+namespace ServerApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SorteioController : ControllerBase
+    {       
+
+        [HttpGet("RealizarSorteio")]
+        public ActionResult<Resultado> RealizarSorteio(string json)
+        {
+            List<Jogo> jogos = JsonConvert.DeserializeObject<List<Jogo>>(json);
+            List<int> listSorteados = new List<int>();
+            Resultado winners = new Resultado();
+            Random rnd = new Random();
+            int number;
+
+            while (listSorteados.Count < 6)
+            {
+                number = rnd.Next(1, 60);
+                if (!listSorteados.Contains(number))
+                    listSorteados.Add(number);
+            }
+
+            winners.NumerosSorteados = listSorteados;
+            winners.NumerosSorteados.Sort();
+
+            foreach (Jogo jogo in jogos)
+            {
+                foreach (int numero in jogo.Numeros)
+                {
+                    if (listSorteados.Contains(numero))
+                        jogo.Acertos++;
+                }
+                jogo.Numeros.Sort();
+
+                if (jogo.Acertos == 4)
+                {                    
+                    winners.Quadra.Add(jogo.Id);
+                }
+                else if (jogo.Acertos == 5)
+                {
+                    winners.Quina.Add(jogo.Id);
+                }
+                else if (jogo.Acertos == 6)
+                {
+                    winners.Sena.Add(jogo.Id);
+                }
+            }
+            return winners;
+        }
+
+        [HttpGet("Test")]
+        public ActionResult<List<Jogo>> Test(string id)
+        {
+            List<Jogo> list = JsonConvert.DeserializeObject<List<Jogo>>(id);
+            return list;
+        }
+
+        [HttpPost, Route("TestPost")]
+        public ActionResult TestPost([FromBody]string id)
+        {
+
+
+            return null;
+        }
+    }
+}
---
+++ ServerApi/Model/Jogo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerApi.Model
+{
+    public class Jogo
+    {
+        public int Id { get; set; }
+        public List<int> Numeros { get; set; }
+        public DateTime Data { get; set; }
+        public int Acertos { get; set; }
+    }
+
+    public class Resultado
+    {
+        public List<int> Quadra { get; set; }
+        public List<int> Quina { get; set; }
+        public List<int> Sena { get; set; }
+        public List<int> NumerosSorteados { get; set; }
+
+        public Resultado()
+        {
+            Quadra = new List<int>();
+            Quina = new List<int>();
+            Sena = new List<int>();
+        }
+    }
+
+    public class Ganhadores
+    {
+        public int Id { get; set; }
+        public string Tipo { get; set; }
+    }
+}
